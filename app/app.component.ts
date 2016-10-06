de0a652e37f735a9cbf83cb48b5932333f28df9c
SagarShelar <sagar.shelar@eternussolutions.com>
2016-10-06 19:00:24 +0530

    Removed assets from app component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-declare var QuickSidebar: any;
-declare var Layout: any;
-declare var App: any;
-import 'assets/layouts/global/scripts/quick-sidebar.js';
-import 'assets/layouts/layout3/scripts/layout.min.js';
-import 'assets/layouts/layout3/scripts/demo.min.js';
+declare var QuickSidebar, Layout, App: any;
 
 @Component({
   selector: 'my-app',
